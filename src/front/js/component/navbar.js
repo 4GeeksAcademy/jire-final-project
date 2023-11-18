cce1a3358eabe4e0a2e3d3a5007a066712ff0235
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-11-18 02:48:27 +0000

    Title modified

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Check a new Navbar</button>
 					</Link>
 				</div>
 			</div>
