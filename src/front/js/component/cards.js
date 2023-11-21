50562788ab8155bbba5b0893445d6822e4a473f8
Isabel Urribarri <93502489+IsabelUrribarri@users.noreply.github.com>
2023-11-21 03:12:15 +0000

    añadiendo estilo a los cards

--- src/front/js/component/cards.js
+++ src/front/js/component/cards.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import '../../styles/cards.css'
+import {Card} from './card'
 
 export const Cards = () => {
   return (
-    <div className="cards-container">
-      <Card buttonText="Encontrar un servicio" />
-      <Card buttonText="Ofrecer un servicio" />
-    </div>
-  );
-}
 
-const Card = ({ buttonText }) => {
-  return (
-    <div className="card">
-      <button>{buttonText}</button>
-    </div>
-  );
+      <div className="cards-container">
+        <Card buttonText="Encontrar un servicio" />
+        <Card buttonText="Ofrecer un servicio" />
+      </div>
+    );
+ 
 }
 
 export default Cards;
