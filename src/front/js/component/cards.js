c52084d9c12b8fb9278f891ba29f19b54a7d64a7
Isabel Urribarri <93502489+IsabelUrribarri@users.noreply.github.com>
2023-11-21 00:11:57 +0000

    cards creados

---
+++ src/front/js/component/cards.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../styles/cards.css'
+
+export const Cards = () => {
+  return (
+    <div className="cards-container">
+      <Card buttonText="Encontrar un servicio" />
+      <Card buttonText="Ofrecer un servicio" />
+    </div>
+  );
+}
+
+const Card = ({ buttonText }) => {
+  return (
+    <div className="card">
+      <button>{buttonText}</button>
+    </div>
+  );
+}
+
+export default Cards;
