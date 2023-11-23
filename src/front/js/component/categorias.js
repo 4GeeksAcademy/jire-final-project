a534cdc058791b8f4bc6c57c4aa9c3f62c9a7f39
Emilio Acosta <139580611+Emilioone@users.noreply.github.com>
2023-11-23 03:23:29 +0000

    Cambios2

--- src/front/js/component/categorias.js
+++ src/front/js/component/categorias.js
@@ -9,17 +9,17 @@ export const Categorias = () => {
             <div class="container mt-4">
                 <div class="row">
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 text-nowrap">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-tools fa-2x mb-2"></i>
-                                <h5 class="card-title">Mantenimiento</h5>
+                                <h5 class="card-title text-nowrap">Mantenimiento</h5>
                             </div>
                         </div>
                     </div>
 
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-broom fa-2x mb-2"></i>
@@ -28,7 +28,7 @@ export const Categorias = () => {
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-building fa-2x mb-2"></i>
@@ -37,7 +37,7 @@ export const Categorias = () => {
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-tree fa-2x mb-2"></i>
@@ -46,7 +46,7 @@ export const Categorias = () => {
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-box-open fa-2x mb-2"></i>
@@ -55,7 +55,7 @@ export const Categorias = () => {
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-laptop fa-2x mb-2"></i>
@@ -64,43 +64,59 @@ export const Categorias = () => {
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-chart-bar fa-2x mb-2"></i>
-                                <h5 class="card-title">Consultoría empresarial</h5>
+                                <h5 class="card-title">Asesoría de negocios</h5>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-leaf fa-2x mb-2"></i>
-                                <h5 class="card-title">Salud y bienestar</h5>
+                                <h5 class="card-title">Salud</h5>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
                                 <i class="fas fa-birthday-cake fa-2x mb-2"></i>
-                                <h5 class="card-title">Eventos y entretenimiento</h5>
+                                <h5 class="card-title text-nowrap">Eventos</h5>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-12 col-md-4 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                         <div class="card text-center">
                             <div class="card-body">
-                                <i class="fas fa-graduation-cap fa-2x mb-2"></i>
-                                <h5 class="card-title">Servicios educativos</h5>
+                                <i class="fas fa-solid fa-language fa-2x mb-2"></i>
+                                <h5 class="card-title">Traducciones</h5>
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <div class="card text-center">
+                            <div class="card-body">
+                                <i class="fas fa-solid fa-book fa-2x mb-2"></i>
+                                <h5 class="card-title">Aprende idiomas</h5>
                             </div>
                         </div>
                     </div>
 
-
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <div class="card text-center">
+                            <div class="card-body">
+                                <i class="fas fa-solid fa-dumbbell fa-2x mb-2"></i>
+                                <h5 class="card-title">Personal Trainning</h5>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
