29c6b5d5670565f7a91902710146f7924d8a8ac7
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-11-25 02:43:15 +0000

    created tables

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Ofertas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=False, nullable=False)
+    description = db.Column(db.Text, unique=False, nullable=False)
+    location = db.Column(db.String(80), unique=False, nullable=False)
 
     def serialize(self):
-        return {
+        return{
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "title": self.title,
+            "description" : self.description,
+            "location": self.location
+        }
+
+
+class Solicitudes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=False, nullable=False)
+    description = db.Column(db.Text, unique=False, nullable=False)
+    location = db.Column(db.String(80), unique=False, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "description" : self.description,
+            "location": self.location
         }
