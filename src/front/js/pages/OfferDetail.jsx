7fd409842eddbf6a77a8e72121f691091309c84c
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-12-14 21:46:01 +0000

    error mail y alert success

--- src/front/js/pages/OfferDetail.jsx
+++ src/front/js/pages/OfferDetail.jsx
@@ -27,9 +27,19 @@ export const OfferDeatail = () => {
             offer_title : offerDetail[3]?.title
         }
         let response = await actions.offerEmail(email)
-        if(response == 200){
-            console.log("se envio")
-        }
+        if (response == 200) {
+            Swal.fire({
+              title: "Perfecto!",
+              text: "Se envio un mail de contacto con tu informacion de contacto al ofertante",
+              icon: "success"
+            });
+          }else{
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Intentalo mas tarde"
+          })
+          }
     }
 
     return (
@@ -47,12 +57,12 @@ export const OfferDeatail = () => {
                     <p className="card-text">{offerDetail[3]?.description}</p>
                     <p className="card-text">{offerDetail[3]?.address}, {offerDetail[3]?.city}, {offerDetail[3]?.state}, {offerDetail[3]?.country}</p>
                     <h5 className="card-title">Datos del profesional</h5>
-                    <p className="card-text">{offerDetail[0]?.name} {offerDetail[0]?.lastname} </p>
-                    <p className="card-text">{offerDetail[2]?.ocupation}</p>
-                    <p className="card-text">{offerDetail[2]?.skills}, {offerDetail[2]?.skills_level} </p>
-                    <p className="card-text">{offerDetail[2]?.languages}</p>
-                    <p className="card-text">{offerDetail[0]?.email}</p>
-                    <p className="card-text">{offerDetail[1]?.phone}</p>
+                    <p className="card-text"><i class="fa-solid fa-user mx-2"></i>{offerDetail[0]?.name} {offerDetail[0]?.lastname} </p>
+                    <p className="card-text"><i class="fa-solid fa-briefcase mx-2"></i>{offerDetail[2]?.ocupation}</p>
+                    <p className="card-text"><i class="fa-solid fa-list-check mx-2"></i>{offerDetail[2]?.skills}, {offerDetail[2]?.skills_level} </p>
+                    <p className="card-text"><i class="fa-solid fa-language mx-2"></i>{offerDetail[2]?.languages}</p>
+                    <p className="card-text"><i class="fa-solid fa-envelope mx-2"></i>{offerDetail[0]?.email}</p>
+                    <p className="card-text"><i class="fa-solid fa-phone mx-2"></i>{offerDetail[1]?.phone}</p>
                     <div className="d-flex ">
                     <Link to={`/public-profile/${userid.userid}/${id.id}`}>
                     <button className="btn btn-primary">Ver Perfil del Profesional</button>
